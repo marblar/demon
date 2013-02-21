cb048d0b51fc7e3c9f5c3f5d46b2913b5aa2439a
Mark Larus <mark@mblar.us>
2013-02-21 07:28:11 -0500

    Implement the 3dscatter.py test.

--- scripts/plot/3dscatter.py
+++ scripts/plot/3dscatter.py
@@ -2,6 +2,10 @@ import csv
 from mpl_toolkits.mplot3d import Axes3D
 import numpy
 import matplotlib
+
+# Run without the window server.
+matplotlib.use('Agg')
+
 import matplotlib.pyplot as plt
 import itertools
 from matplotlib import cm
@@ -9,6 +13,13 @@ from matplotlib.ticker import LinearLocator, FormatStrFormatter
 from math import log
 from math import atan as arctan
 from sys import stdin
+import argparse
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("name")
+args = parser.parse_args()
 
 reader = csv.reader(stdin)
 values = itertools.chain(reader)
@@ -34,8 +45,11 @@ def drange(start, stop, step):
 
 col = numpy.array(map(RYB,map(colorBy,z)))
 
-fig = plt.figure()
-ax = Axes3D(fig)
+fig = plt.figure(figsize=(10,5))
+fig.suptitle(args.name,fontsize=14,fontweight='bold')
+ax = fig.add_subplot(111, projection='3d')
+
+ax.set_title('<J>=%s' % round(numpy.mean(z),2),position=(0,0),ha='center')
 
 p = ax.scatter(x,y,z,s=12,vmin=-1,vmax=1,c=col, cmap=RYB)
 fig.add_axes(ax)
@@ -48,8 +62,8 @@ ax.set_ylim(0,1)
 ax.set_zlim(-1,5)
 
 ax.set_xlabel(u"\u2202")
-ax.set_ylabel(u'\u03F5')
-ax.set_zlabel('<J>')
+ax.set_ylabel(u"\u0395")
+ax.set_zlabel('J')
 
 print "Axes:"
 print "%s: (%s)" % (header[0],ax.get_xlim())
@@ -58,5 +72,7 @@ print "%s: (%s)" % (header[2],ax.get_zlim())
 #fig.show()
 #import time
 #time.sleep(10)
+
+
 
 fig.savefig("plot.png")
--- scripts/test/test-plot.sh
+++ scripts/test/test-plot.sh
@@ -1,4 +1,4 @@
 #!/usr/bin -ex
 
 #TODO: Use autotools to find python27
-python ../plot/make-graph.py $NAME < ../plot/result.csv
+python ../plot/3dscatter.py $BUILD_TAG < ../plot/test.csv
