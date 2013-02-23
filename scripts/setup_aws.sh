d7295c7b5d90ab84394bebb557c101e39bc66772
Mark Larus <mark@mblar.us>
2013-02-22 22:42:38 -0500

    Initialize the jenkins root in the aws setup script.

---
+++ scripts/setup_aws.sh
@@ -0,0 +1,4 @@
+sudo apt-get update && sudo apt-get install g++ libgsl10-dev libboost-program-options1.50-dev libbost-system1.50-dev bash make
+sudo mkdir /var/mblar
+sudo chown ubuntu /var/mblar
+echo BOOST_ROOT=/usr >> ~/.bashrc
