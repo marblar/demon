da9623e27cc6bb4c49833cc015b2bd442e925c69
Mark Larus <mark@mblar.us>
2013-02-24 19:22:35 -0500

    Correct some improprieties with the setup scripts.

--- scripts/setup_aws.sh
+++ scripts/setup_aws.sh
@@ -1,4 +1,4 @@
-sudo apt-get update && sudo apt-get install g++ libgsl10-dev libboost-program-options1.50-dev libbost-system1.50-dev bash make
+sudo apt-get update && sudo apt-get install g++ libgsl0-dev libboost-program-options1.50-dev libboost-system1.50-dev bash make matplotlib
 sudo mkdir /var/mblar
 sudo chown ubuntu /var/mblar
 echo BOOST_ROOT=/usr >> ~/.bashrc
