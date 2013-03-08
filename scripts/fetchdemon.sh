50b55de11cac17305f71429e902c4d5d9b870248
Mark Larus <mark@mblar.us>
2013-03-08 12:06:40 -0500

    Add starcluster download and build script.

---
+++ scripts/fetchdemon.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+curl http://physics.mblar.us/view/Builds/job/package-demon/lastSuccessfulBuild/artifact/build/demon-0.1.tar.gz | tar xz
+cd demon-0.1
+mkdir -p build
+cd build
+../configure
+make
+qrsh -b y -cwd -pe orte 2 mpirun jdemon-mpi
