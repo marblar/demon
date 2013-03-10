7bcd073c33d4f5df3dd23b4e2f6dd869c0f57e47
Mark Larus <mark@mblar.us>
2013-03-10 10:12:36 -0400

    Test tarball url before starting a test cluster.

--- scripts/jenkins/test-deploy.sh
+++ scripts/jenkins/test-deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# Test the URL before we start the cluster.
+
+DOWNSTREAM_URL=`curl $JENKINS_URL/job/package-demon/api/xml?depth=1&xpath=/freeStyleProject/build%5Baction/cause/upstreamProject=%22$UPSTREAM_JOB_NAME%22%20and%20action/cause/upstreamBuild=$UPSTREAM_BUILD_ID%5D/url/text()`
+DOWNSTREAM_FILENAME=`curl $JENKINS_URL/job/package-demon/api/xml?depth=1&xpath=/freeStyleProject/build%5Baction/cause/upstreamProject=%22$UPSTREAM_JOB_NAME%22%20and%20action/cause/upstreamBuild=$UPSTREAM_BUILD_ID%5D/artifact/fileName/text()`
+TARBALL_URL=$DOWNSTREAM_URL/artifact/build/$DOWNSTREAM_FILENAME
+
+curl $TARBALL_URL | tar xz
+
 starcluster start -c testcluster --force-spot-master -b .005 $BUILD_TAG
 starcluster put $BUILD_TAG -u mark scripts/fetchdemon.sh fetchdemon.sh
 starcluster put $BUILD_TAG -u mark *.tar.gz *.tar.gz
