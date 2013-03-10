1a126b5a1c4a4487bed31238f4fd2b9d481438b1
Mark Larus <mark@mblar.us>
2013-03-10 09:49:51 -0400

    Update test deployment script to reflect new filename.

--- scripts/fetchdemon.sh
+++ scripts/fetchdemon.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-curl http://physics.mblar.us/view/Builds/job/package-demon/lastSuccessfulBuild/artifact/build/demon-0.1.tar.gz | tar xz
-cd demon-0.1
+curl $1 | tar xz
+cd jdemon-*
 mkdir -p build
 cd build
 ../configure
 make
-qrsh -b y -cwd -pe orte 2 mpirun jdemon-mpi --iterations 1000
+qrsh -b y -cwd -pe orte 2 mpirun jdemon-mpi --iterations 100
--- scripts/jenkins/test-deploy.sh
+++ scripts/jenkins/test-deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 starcluster start -c testcluster --force-spot-master -b .005 $BUILD_TAG
 starcluster put $BUILD_TAG -u mark scripts/fetchdemon.sh fetchdemon.sh
-starcluster sshmaster $BUILD_TAG -u mark ./fetchdemon.sh
+starcluster put $BUILD_TAG -u mark *.tar.gz *.tar.gz
+
+DOWNSTREAM_URL=`curl $JENKINS_URL/job/package-demon/api/xml?depth=1&xpath=/freeStyleProject/build%5Baction/cause/upstreamProject=%22$JOB_NAME%22%20and%20action/cause/upstreamBuild=$BUILD_ID%5D/url/text()`
+DOWNSTREAM_FILENAME=`curl $JENKINS_URL/job/package-demon/api/xml?depth=1&xpath=/freeStyleProject/build%5Baction/cause/upstreamProject=%22$JOB_NAME%22%20and%20action/cause/upstreamBuild=$BUILD_ID%5D/artifact/fileName/text()`
+
+TARBALL_URL=$DOWNSTREAM_URL/artifact/build/$DOWNSTREAM_FILENAME
+
+starcluster sshmaster $BUILD_TAG -u mark ./fetchdemon.sh $TARBALL_URL
 
 # Be sure to execute test-deploy-cleanup.sh to terminate the new cluster.
