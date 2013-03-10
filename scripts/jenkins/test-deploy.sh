84fef81077f7af0f11e9ec2709c68cd92bc880e7
Mark Larus <mark@mblar.us>
2013-03-10 10:59:26 -0400

    Stop trying to put the tarball manually.

--- scripts/jenkins/test-deploy.sh
+++ scripts/jenkins/test-deploy.sh
@@ -4,14 +4,12 @@
 
 DOWNSTREAM_URL=`curl "$JENKINS_URL/job/package-demon/api/xml?depth=1&xpath=/freeStyleProject/build%5Baction/cause/upstreamProject=%22$UPSTREAM_JOB_NAME%22%20and%20action/cause/upstreamBuild=$UPSTREAM_BUILD_ID%5D/url/text()"`
 DOWNSTREAM_FILENAME=`curl "$JENKINS_URL/job/package-demon/api/xml?depth=1&xpath=/freeStyleProject/build%5Baction/cause/upstreamProject=%22$UPSTREAM_JOB_NAME%22%20and%20action/cause/upstreamBuild=$UPSTREAM_BUILD_ID%5D/artifact/fileName/text()"`
-TARBALL_URL=$DOWNSTREAM_URL/artifact/build/$DOWNSTREAM_FILENAME
+TARBALL_URL=${DOWNSTREAM_URL}artifact/build/$DOWNSTREAM_FILENAME
 
 curl $TARBALL_URL | tar xz
 
 starcluster start -c testcluster --force-spot-master -b .005 $BUILD_TAG
 starcluster put $BUILD_TAG -u mark scripts/fetchdemon.sh fetchdemon.sh
-starcluster put $BUILD_TAG -u mark *.tar.gz *.tar.gz
-
 starcluster sshmaster $BUILD_TAG -u mark ./fetchdemon.sh $TARBALL_URL
 
 # Be sure to execute test-deploy-cleanup.sh to terminate the new cluster.
