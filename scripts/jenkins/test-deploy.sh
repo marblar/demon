900e46cb8eccb60ea300ab19b0e83575169918fa
Mark Larus <mark@mblar.us>
2013-03-10 10:09:15 -0400

    Use UPSTREAM_BUILD_ID instead of  in test deployment.

--- scripts/jenkins/test-deploy.sh
+++ scripts/jenkins/test-deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+
 starcluster start -c testcluster --force-spot-master -b .005 $BUILD_TAG
 starcluster put $BUILD_TAG -u mark scripts/fetchdemon.sh fetchdemon.sh
 starcluster put $BUILD_TAG -u mark *.tar.gz *.tar.gz
 
-DOWNSTREAM_URL=`curl $JENKINS_URL/job/package-demon/api/xml?depth=1&xpath=/freeStyleProject/build%5Baction/cause/upstreamProject=%22$JOB_NAME%22%20and%20action/cause/upstreamBuild=$BUILD_ID%5D/url/text()`
-DOWNSTREAM_FILENAME=`curl $JENKINS_URL/job/package-demon/api/xml?depth=1&xpath=/freeStyleProject/build%5Baction/cause/upstreamProject=%22$JOB_NAME%22%20and%20action/cause/upstreamBuild=$BUILD_ID%5D/artifact/fileName/text()`
+DOWNSTREAM_URL=`curl $JENKINS_URL/job/package-demon/api/xml?depth=1&xpath=/freeStyleProject/build%5Baction/cause/upstreamProject=%22$UPSTREAM_JOB_NAME%22%20and%20action/cause/upstreamBuild=$UPSTREAM_BUILD_ID%5D/url/text()`
+DOWNSTREAM_FILENAME=`curl $JENKINS_URL/job/package-demon/api/xml?depth=1&xpath=/freeStyleProject/build%5Baction/cause/upstreamProject=%22$UPSTREAM_JOB_NAME%22%20and%20action/cause/upstreamBuild=$UPSTREAM_BUILD_ID%5D/artifact/fileName/text()`
 
 TARBALL_URL=$DOWNSTREAM_URL/artifact/build/$DOWNSTREAM_FILENAME
 
