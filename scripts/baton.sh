e812a3173cd511c1005f3a4a2fb5da72d0410998
Mark Larus <mark@mblar.us>
2013-03-11 08:59:01 -0400

    Better use of positional arguments in baton.

--- scripts/baton.sh
+++ scripts/baton.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 FOLDER=$(basename $1 .tar.gz)
 tar xzf $1
+shift
 cd $FOLDER
 mkdir -p build
 cd build
 ../configure
 make
-mpirun -np $2 jdemon-mpi ${@:2} | tee result.csv
-python ../scripts/plot/3dscatter.py "${FOLDER}:jdemon-mpi ${@:2}" < result.csv
+PROCESSES=$1
+shift
+mpirun -np $PROCESSES jdemon-mpi $@ | tee result.csv
+python ../scripts/plot/3dscatter.py '${FOLDER}:jdemon-mpi $@' < result.csv
 mv result.csv ../..
 mv plot.png ../..
