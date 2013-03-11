7f4aea085140589436859fdc4b6f0fe8a76ebd5e
Mark Larus <mark@mblar.us>
2013-03-11 08:21:01 -0400

    Provide name of tarball to the baton when creating a tiny-cluster.

--- scripts/jenkins/tiny-cluster.sh
+++ scripts/jenkins/tiny-cluster.sh
@@ -3,9 +3,9 @@
 # Test the URL before we start the cluster.
 
 starcluster start -c testcluster --force-spot-master -b .005 $BUILD_TAG
-starcluster put $BUILD_TAG -u mark $TARBALL jdemon.tar.gz
+starcluster put $BUILD_TAG -u mark $TARBALL $TARBALL
 starcluster put $BUILD_TAG -u mark $FOLDER/scripts/baton.sh baton.sh
-starcluster sshmaster $BUILD_TAG -u mark "./baton.sh 2 --iterations 1000"
+starcluster sshmaster $BUILD_TAG -u mark "./baton.sh $TARBALL 2 --iterations 1000"
 starcluster get $BUILD_TAG -u mark result.csv result.csv
 starcluster get $BUILD_TAG -u mark plot.png plot.png
 
