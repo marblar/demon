c072f2affc60d5bb09a3ce086dfe8abc1a48e86a
Mark Larus <mark@mblar.us>
2013-03-11 00:51:19 -0400

    Escape tiny-cluster remote arguments.

--- scripts/jenkins/tiny-cluster.sh
+++ scripts/jenkins/tiny-cluster.sh
@@ -5,8 +5,8 @@
 starcluster start -c testcluster --force-spot-master -b .005 $BUILD_TAG
 starcluster put $BUILD_TAG -u mark $TARBALL jdemon.tar.gz
 starcluster put $BUILD_TAG -u mark $FOLDER/scripts/baton.sh baton.sh
-starcluster sshmaster $BUILD_TAG -u mark ./baton.sh 2 --iterations 1000
+starcluster sshmaster $BUILD_TAG -u mark "./baton.sh 2 --iterations 1000"
 starcluster get $BUILD_TAG -u mark result.csv result.csv
 starcluster get $BUILD_TAG -u mark plot.png plot.png
 
-# Be sure to execute test-deploy-cleanup.sh to terminate the new cluster.
+# Be sure to execute tiny-cluster-cleanup.sh to terminate the new cluster.
