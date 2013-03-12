38c8f092af43220ede6535583a16e6f4979d2b21
Mark Larus <mark@mblar.us>
2013-03-12 18:24:21 -0400

    Use all 32*9 available slots.

--- scripts/jenkins/deploy-aws.sh
+++ scripts/jenkins/deploy-aws.sh
@@ -3,7 +3,7 @@
 starcluster start -c deploy aws-deployment 
 starcluster put aws-deployment -u mark $TARBALL $TARBALL
 starcluster put aws-deployment -u mark $FOLDER/scripts/baton.sh baton.sh 
-starcluster sshmaster aws-deployment -u mark "./baton.sh $TARBALL 250 --iterations 16777216"
+starcluster sshmaster aws-deployment -u mark "./baton.sh $TARBALL 288 --iterations 16777216"
 starcluster get aws-deployment -u mark result.csv result.csv
 starcluster get aws-deployment -u mark plot.png plot.png
 
