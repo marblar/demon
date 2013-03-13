41c55c4de94843b0d25971e24031045da0cf5c5b
Mark Larus <mark@mblar.us>
2013-03-12 21:21:22 -0400

    Use  in configure scripts.

--- scripts/jenkins/deploy-kenyon.sh
+++ scripts/jenkins/deploy-kenyon.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir -p build
 cd build
-../configure --with-boost=$BOOST_ROOT
+../configure $BOOST_FLAGS
 make
 mpirun -np 16 jdemon-mpi $ARGS | tee result.csv
 python ../scripts/plot/3dscatter.py plot.png < result.csv
--- scripts/jenkins/short-run.sh
+++ scripts/jenkins/short-run.sh
@@ -2,7 +2,7 @@
 cd $FOLDER
 mkdir -p build
 cd build
-../configure --with-boost=$BOOST_ROOT
+../configure $BOOST_FLAGS
 make
 
 INVOCATION="mpirun -np 8 jdemon-mpi --stoch -n 262144"
@@ -13,7 +13,7 @@ PLOT_SCRIPT=../scripts/plot/3dscatter.py
 $INVOCATION | tee $OUTPUT_FILE
 python $PLOT_SCRIPT stoch-plot.png  < $OUTPUT_FILE
 
-INVOCATION="mpirun -np 8 jdemon-mpi --ising -n 4096 -d 20 --clusters 20"
+INVOCATION="mpirun -np 16 jdemon-mpi --ising -n 4096 -d 20 --clusters 20"
 OUTPUT_FILE=ising-result.csv
 
 #Perform the ising run
