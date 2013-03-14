d482dfaead8197285ee1313da7e2769cc1f3c87a
Mark Larus <mark@mblar.us>
2013-03-14 08:46:36 -0500

    Use BOOST_CHECK instead of BOOST_REQUIRE where possible.
    An early require might block useful debugging information from a later check.

--- test/lib/IsingGridTest.cpp
+++ test/lib/IsingGridTest.cpp
@@ -9,7 +9,8 @@
 #include <boost/test/unit_test.hpp>
 #include <algorithm>
 #include <set>
-#include <vector>
+#include <sstream>
+#include <map>
 
 #include "IsingGrid.h"
 
@@ -22,31 +23,30 @@ struct IsingGridFixture {
     IsingGridFixture() : grid(6) {}
 };
 
-template<class Set1, class Set2>
-bool is_disjoint(const Set1 &set1, const Set2 &set2)
+Cell* is_disjoint(const CellSet &set1, const CellSet &set2)
 {
-    if(set1.empty() || set2.empty()) return true;
+    if(set1.empty() || set2.empty()) return NULL;
     
-    typename Set1::const_iterator
-    it1 = set1.begin(),
+    CellSet::const_iterator it1 = set1.begin(),
     it1End = set1.end();
-    typename Set2::const_iterator
-    it2 = set2.begin(),
+    CellSet::const_iterator it2 = set2.begin(),
     it2End = set2.end();
     
-    if(*it1 > *set2.rbegin() || *it2 > *set1.rbegin()) return true;
+    if(*it1 > *set2.rbegin() || *it2 > *set1.rbegin()) return NULL;
     
     while(it1 != it1End && it2 != it2End)
     {
-        if(*it1 == *it2) return false;
+        if(*it1 == *it2) {
+            return *it1;
+        }
         if(*it1 < *it2) { it1++; }
         else { it2++; }
     }
     
-    return true;
+    return NULL;
 }
 
-BOOST_FIXTURE_TEST_SUITE(DetailedIsingGridTest, IsingGridFixture)
+BOOST_FIXTURE_TEST_SUITE(IsingGridTest, IsingGridFixture)
 
 BOOST_AUTO_TEST_CASE( testEvenOddIteratorsDisjoint ) {
     CellSet evenSet;
@@ -56,13 +56,13 @@ BOOST_AUTO_TEST_CASE( testEvenOddIteratorsDisjoint ) {
     size_t count = 0;
     
     for (CellSet::iterator it = evenSet.begin(); it!=evenSet.end(); ++it) {
-        BOOST_REQUIRE(!oddSet.count(*it));
+        BOOST_CHECK(!oddSet.count(*it));
         ++count;
         BOOST_REQUIRE_LE(count, grid.size());
     }
     count = 0;
     for (CellSet::iterator it = oddSet.begin(); it!=oddSet.end(); ++it) {
-        BOOST_REQUIRE(!evenSet.count(*it));
+        BOOST_CHECK(!evenSet.count(*it));
         ++count;
         BOOST_REQUIRE_LE(count, grid.size());
     }
@@ -105,11 +105,11 @@ BOOST_AUTO_TEST_CASE( testAllCellsInEvenOdd ) {
     allSet.insert(grid.begin(),grid.end());
     
     for (CellSet::iterator it = allSet.begin(); it!=allSet.end(); ++it) {
-        BOOST_REQUIRE(evenOddSet.count(*it));
+        BOOST_CHECK(evenOddSet.count(*it));
     }
     
     for (CellSet::iterator it = evenOddSet.begin(); it!=evenOddSet.end(); ++it) {
-        BOOST_REQUIRE(allSet.count(*it));
+        BOOST_CHECK(allSet.count(*it));
     }
 }
 
@@ -119,11 +119,11 @@ BOOST_AUTO_TEST_CASE( testUniqueNeighbors ) {
         Cell::Neighbors neighbors = (*it)->getNeighbors();
         CellSet neighborSet(neighbors.begin(),neighbors.end());
         
-        BOOST_REQUIRE_EQUAL(neighborSet.size(), neighbors.size());
+        BOOST_CHECK_EQUAL(neighborSet.size(), neighbors.size());
         
         // I wouldn't consider this an important unit test
         // But we might want to know if this changed:
-        BOOST_REQUIRE_EQUAL(neighbors.size(), 4);
+        BOOST_CHECK_EQUAL(neighbors.size(), 4);
         BOOST_REQUIRE_LE(loops, grid.size());
     }
 }
@@ -139,7 +139,11 @@ BOOST_AUTO_TEST_CASE( testNoNeighborsInOdd ) {
         evenNeighbors.insert(neighbors.begin(),neighbors.end());
         BOOST_REQUIRE_LE(loopGuard, grid.size());
     }
-    BOOST_REQUIRE(is_disjoint(evenNeighbors,evenCells));
+    Cell *evenIntersection = is_disjoint(evenNeighbors,evenCells);
+    size_t offset = evenIntersection - *grid.begin();
+    BOOST_CHECK(evenIntersection==NULL);
+    
+    BOOST_CHECK(!oddNeighbors.count(*grid.begin()));
     
     loopGuard = 0;
     for (Grid::subset::iterator it = grid.odds.begin(); it!=grid.odds.end(); ++it) {
@@ -149,11 +153,11 @@ BOOST_AUTO_TEST_CASE( testNoNeighborsInOdd ) {
         oddNeighbors.insert(neighbors.begin(),neighbors.end());
         BOOST_REQUIRE_LE(loopGuard, grid.size());
     }
-
-    BOOST_REQUIRE(is_disjoint(oddNeighbors, oddCells));
+    Cell * oddIntersection = is_disjoint(oddNeighbors, oddCells);
+    BOOST_CHECK(oddIntersection==NULL);
     
-    BOOST_REQUIRE_EQUAL_COLLECTIONS(evenNeighbors.begin(), evenNeighbors.end(), oddCells.begin(), oddCells.end());
-    BOOST_REQUIRE_EQUAL_COLLECTIONS(oddNeighbors.begin(), oddNeighbors.end(), evenCells.begin(), evenCells.end());
+    BOOST_CHECK_EQUAL_COLLECTIONS(evenNeighbors.begin(), evenNeighbors.end(), oddCells.begin(), oddCells.end());
+    BOOST_CHECK_EQUAL_COLLECTIONS(oddNeighbors.begin(), oddNeighbors.end(), evenCells.begin(), evenCells.end());
 }
 
 BOOST_AUTO_TEST_CASE( gridSize ) {
@@ -173,7 +177,7 @@ BOOST_AUTO_TEST_CASE( testNeighborsTransitive ) {
         for (Cell::Neighbors::iterator neighbor = neighbors.begin(); neighbor!=neighbors.end(); ++neighbor) {
             Cell::Neighbors reverseNeighbors = (*neighbor)->getNeighbors();
             long count = std::count(reverseNeighbors.begin(), reverseNeighbors.end(), *it);
-            BOOST_REQUIRE_EQUAL(count, 1);
+            BOOST_CHECK_EQUAL(count, 1);
             BOOST_REQUIRE_LE(innerLoops, neighbors.size());
             ++innerLoops;
         }
@@ -185,9 +189,9 @@ BOOST_AUTO_TEST_CASE( testNeighborsTransitive ) {
 BOOST_AUTO_TEST_CASE( testSetValue ) {
     Cell cell;
     cell.setValue(0);
-    BOOST_REQUIRE_EQUAL(cell.getValue(), 0);
+    BOOST_CHECK_EQUAL(cell.getValue(), 0);
     cell.setValue(1);
-    BOOST_REQUIRE_EQUAL(cell.getValue(), 1);
+    BOOST_CHECK_EQUAL(cell.getValue(), 1);
     BOOST_REQUIRE_THROW(cell.setValue(2), InvalidCellValue);
 }
 
@@ -203,4 +207,13 @@ BOOST_AUTO_TEST_CASE( testRandomAccess ) {
     BOOST_REQUIRE_THROW(grid[2*size], InvalidGridIndex);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_CASE( testCoordinateNeighbors ) {
+    Coordinate center(2,2,grid.getDimension());
+    Coordinate north(2,3,grid.getDimension());
+    Coordinate south(2,1,grid.getDimension());
+    Coordinate east(3,2,grid.getDimension());
+    Coordinate west(1,2,grid.getDimension());
+    
+}
+
+BOOST_AUTO_TEST_SUITE_END() 
