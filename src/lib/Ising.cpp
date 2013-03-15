99c48ca521416075b74b3a006f85c6bee3a1d772
Mark Larus <mark@mblar.us>
2013-03-15 09:35:10 -0700

    Formatting change.

--- src/lib/Ising.cpp
+++ src/lib/Ising.cpp
@@ -37,7 +37,8 @@ Reservoir::InteractionResult IsingReservoir::interactWithBit(int bit) {
 void IsingReservoir::isingStep(InteractionResult &result) {
     Grid::subset::iterator it = currentStepType ? \
         grid.odds.begin() : grid.evens.begin();
-    Grid::subset::iterator end = currentStepType ? grid.odds.end() : grid.evens.end();
+    Grid::subset::iterator end = currentStepType ? \
+        grid.odds.end() : grid.evens.end();
     for (;it!=end; ++it) {
         Cell *cell = *it;
         int energy = (int)cell->getEnergy();
