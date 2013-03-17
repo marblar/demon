7b4aafdb75c93513a2cb099a7a5fdde14f185374
Mark Larus <mark@mblar.us>
2013-03-17 16:24:19 -0700

    ./jdemon-mpi instead of jdemon-mpi on short run.

--- scripts/jenkins/short-run.sh
+++ scripts/jenkins/short-run.sh
@@ -5,7 +5,7 @@ cd build
 ../configure $BOOST_FLAGS
 make
 
-INVOCATION="mpirun -np $MAX_PROCESSES jdemon-mpi --stoch -n 262144"
+INVOCATION="mpirun -np $MAX_PROCESSES ./jdemon-mpi --stoch -n 262144"
 OUTPUT_FILE=stoch-result.csv
 PLOT_SCRIPT=../scripts/plot/3dscatter.py
 
@@ -13,7 +13,7 @@ PLOT_SCRIPT=../scripts/plot/3dscatter.py
 $INVOCATION | tee $OUTPUT_FILE
 python $PLOT_SCRIPT stoch-plot.png  < $OUTPUT_FILE
 
-INVOCATION="mpirun -np $MAX_PROCESSES jdemon-mpi --ising -n 4096 -d 20 --clusters 20"
+INVOCATION="mpirun -np $MAX_PROCESSES ./jdemon-mpi --ising -n 4096 -d 20 --clusters 20"
 OUTPUT_FILE=ising-result.csv
 
 #Perform the ising run
