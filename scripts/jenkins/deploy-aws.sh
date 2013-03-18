cd1a18370a24f60665504b6a95aceaabadadc009
Mark Larus <mark@mblar.us>
2013-03-18 07:50:24 -0700

    Add parameters to AWS deployment.

--- scripts/jenkins/deploy-aws.sh
+++ scripts/jenkins/deploy-aws.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-starcluster start -c deploy aws-deployment 
+starcluster start -c deploy -s $CLUSTER_SIZE aws-deployment 
 starcluster put aws-deployment -u mark $TARBALL $TARBALL
 starcluster put aws-deployment -u mark $FOLDER/scripts/baton.sh baton.sh 
-starcluster sshmaster aws-deployment -u mark "./baton.sh $TARBALL 288 --iterations 16777216"
+starcluster sshmaster aws-deployment -u mark "./baton.sh $TARBALL $PROCESSES $ARGS"
 starcluster get aws-deployment -u mark result.csv result.csv
 starcluster get aws-deployment -u mark plot.png plot.png
 
