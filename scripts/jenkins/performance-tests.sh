484f8dfc123549ec612b2f284087f96df5b916e1
Mark Larus <mark@mblar.us>
2013-03-25 19:27:50 -0400

    Perfomance tests build only marked as failure if there's no build stats files.

--- scripts/jenkins/performance-tests.sh
+++ scripts/jenkins/performance-tests.sh
@@ -4,3 +4,5 @@ mkdir -p build
 cd build
 ../configure --enable-performance-tests --cache-file=$HOME/demon-cache
 make check
+PERFORMANCE_DIR=build/test/performance
+test $PERFORMANCE_DIR/ising.stats $PERFORMANCE_DIR/stoch.stats
