ce4ba18b86e8f95752025c6a016278e4249bb677
Mark Larus <mark@mblar.us>
2013-04-14 00:59:35 -0400

    Add TMGas to the staging phase. Closes #11.

--- scripts/jenkins/short-run.sh
+++ scripts/jenkins/short-run.sh
@@ -5,17 +5,19 @@ cd build
 ../configure $BOOST_FLAGS
 make
 
-INVOCATION="mpirun -np $MAX_PROCESSES ./jdemon-mpi --stoch -n 262144"
-OUTPUT_FILE=stoch-result.csv
 PLOT_SCRIPT=../scripts/plot/3dscatter.py
 
-# Perform the stochastic run
+INVOCATION="mpirun -np $MAX_PROCESSES ./jdemon-mpi --stoch -n 262144"
+OUTPUT_FILE=stoch-result.csv
 $INVOCATION | tee $OUTPUT_FILE
 python $PLOT_SCRIPT stoch-plot.png  < $OUTPUT_FILE
 
 INVOCATION="mpirun -np $MAX_PROCESSES ./jdemon-mpi --ising -n 4096 -d 20 --clusters 20"
 OUTPUT_FILE=ising-result.csv
-
-#Perform the ising run
 $INVOCATION | tee $OUTPUT_FILE
 python $PLOT_SCRIPT ising-plot.png < $OUTPUT_FILE
+
+INVOCATION="mpirun -np $MAX_PROCESSES ./jdemon-mpi --tmgas -n 4096 -d 16"
+OUTPUT_FILE=tmgas-result.csv
+$INVOCATION | tee $OUTPUT_FILE
+python $PLOT_SCRIPT tmgas-plot.png < $OUTPUT_FILE
